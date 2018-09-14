16d786c03866109909da40da1913a685146a41a3
chenrui <chenrui@chenruitekimacbook-air.local>
2018-09-14 16:38:28 +0800

    Initial Commit

---
+++ ordernumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ordernumber
+//
+//  Created by chenrui on 2018/9/14.
+//  Copyright © 2018年 Andy Chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
