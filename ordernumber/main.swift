a1ce2f954b7beb5a2552eb60326a48019b694e22
Andy Chen <chenrui@chenruitekiMacBook-Air.local>
2018-09-19 19:20:10 +0800

    the way of showing

--- ordernumber/main.swift
+++ ordernumber/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
+var n:[Int] = [1,3,2,5,4,9,3,29,4,7]
+var temp:Int
+var title:String
+
+title = "after order:"
+
+print(n)
+
+for index in 0...9 {
+    for index2 in 0...9 {
+        if n[index] < n[index2] {
+            temp = n[index]
+            n[index] = n[index2]
+            n[index2] = temp
+        }
+    }
+}
+
+print(title)
+
+print(n)
